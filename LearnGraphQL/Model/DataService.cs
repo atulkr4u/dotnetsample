8575ee6070c0d24e05ccceff2c6f1fed5e0804c9
Atul Kumar <atulkr4u@hotmail.com>
2023-06-18 09:07:52 +0530

    Added Graph QL

---
+++ LearnGraphQL/Model/DataService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace LearnGraphQL.Model
+{
+	public class Customer
+	{
+		public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public int Pincode { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+    }
+    public class SearchCustomer
+    {
+        public int? CustomerId { get; set; }
+        public string CustomerNameKeyWord { get; set; }
+        public int? Pincode { get; set; }
+    }
+    public  class CustomerDb
+    {
+        public  List<Customer> SearchCustomer(SearchCustomer searchCustomer)
+        {
+            List<Customer> _customers = new List<Customer>() {
+            new Customer { CustomerId=1, Address = "ABC", City="Delhi", CustomerName = "Atul Kumar", Pincode= 110091 },
+            new Customer { CustomerId=2, Address="XYZ", City="Lucknow", CustomerName = "Shobha Rastogi", Pincode=226016 } };
+
+            var result = new List<Customer>();
+            if (searchCustomer.CustomerId.HasValue)
+            {
+               result.Add(_customers.FirstOrDefault(t => t.CustomerId == searchCustomer.CustomerId));
+            }
+            if(!string.IsNullOrEmpty(searchCustomer.CustomerNameKeyWord))
+            {
+                var search = _customers.Where(t => t.CustomerName.Contains(searchCustomer.CustomerNameKeyWord));
+                result.AddRange(search);
+            }
+            if (searchCustomer.Pincode.HasValue &&  searchCustomer.Pincode > 0)
+            {
+                var search = _customers.Where(t => t.Pincode == searchCustomer.Pincode);
+                result.AddRange(search);
+            }
+            return result;
+        }
+
+    }
+}
+
---
+++ LearnGraphQL/Program.cs
@@ -0,0 +1,49 @@
+﻿using LearnGraphQL.Model;
+
+namespace LearnGraphQL;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddGraphQLServer();
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+
+
+        app.MapControllers();
+        // Configure the HTTP request pipeline.
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGraphQL();
+        });
+        app.Run();
+    }
+}
+
+//Sample Query:
+//    query {
+//  searchCustomer(searchCustomer: { customerNameKeyWord: "At" }) {
+//    customerId
+//    city
+//  }
+//}
